a4fd7ea3bcdf2ba26f9d08c748a982f03d5b5eb9
vinoddotcom <vinod.yadav@vidyamantra.com>
2023-08-18 12:37:40 +0530

    dfg

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    hostRules :[
+      {
+        hostType: 'npm',
+        baseUrl: "https://github.com/exampathfinder/epf-auth-layer/pkgs/npm/auth-layer",
+        token: ${process.env.PRIVATE_NPM_TOKEN},
+        authType: "Basic"
+      },
+    ],
+    "packageRules": [
+        {
+           "matchUpdateTypes": ["patch", "minor"],
+           "matchPackagePatterns": ["@exampathfinder/auth-layer"]
+        }
+       ],
+    npmrc : `//npm.pkg.github.com/:_authToken=${process.env.PRIVATE_NPM_TOKEN}`  
+}
